ff63442dfdf14b5df2fb5c5ae2d7692d55ea9cc1
Lucas Assunção <lucas.9592@gmail.com>
2024-08-21 17:32:09 -0300

    Adding struct to the project

--- main.go
+++ main.go
@@ -15,7 +15,34 @@ import (
 	"github.com/logrusorgru/aurora/v4"
 )
 
-var dir string
+type ConfigurationManager struct {
+	Config       *configparser.Configuration
+	SectionNames []string
+	TabbyWriter  *tabby.Tabby
+	TabbyFile    string
+	Dir          string
+	LogType      string
+}
+
+func NewConfigManager(tabbyFile, logType string) (*ConfigurationManager, error) {
+	config, sectionNames := getConfSections()
+
+	fd, err := os.OpenFile(tabbyFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	w := tabwriter.NewWriter(fd, 0, 0, 4, ' ', 0)
+	t := tabby.NewCustom(w)
+
+	return &ConfigurationManager{
+		Config:       config,
+		SectionNames: sectionNames,
+		TabbyWriter:  t,
+		TabbyFile:    tabbyFile,
+		LogType:      logType,
+	}, nil
+}
 
 func getConfSections() (*configparser.Configuration, []string) {
 	configparser.Delimiter = "="
@@ -23,10 +50,10 @@ func getConfSections() (*configparser.Configuration, []string) {
 	config, err := configparser.Read(types.ConfigFile)
 	if err != nil {
 		logging.Log.Errorf("Failed to read configuration file: %s. Error: %s.", types.ConfigFile, err.Error())
+		return nil, nil
 	}
 
 	sections, err := config.AllSections()
-
 	if err != nil {
 		logging.Log.Errorf("Unable to read sections from the configuration file. Error: %s.", err.Error())
 		return nil, nil
@@ -39,57 +66,58 @@ func getConfSections() (*configparser.Configuration, []string) {
 	return config, availableSectionNames
 }
 
-func moveFileAndAddLineToTabbyLog(files []fs.DirEntry, t *tabby.Tabby, entry, src string) {
-	t.AddHeader("DATE", "TIME", "TYPE", "SOURCE", "DESTINATION", "NAME", "SIZE", "STATUS")
+func (cm *ConfigurationManager) moveFileAndAddLineToTabbyLog(files []fs.DirEntry, entry, src string) {
+	cm.TabbyWriter.AddHeader("DATE", "TIME", "TYPE", "SOURCE", "DESTINATION", "NAME", "SIZE", "STATUS")
 	for _, file := range files {
 		fileInfo, _ := file.Info()
 
 		if strings.HasSuffix(file.Name(), strings.ToUpper("."+entry)) || strings.HasSuffix(file.Name(), strings.ToLower("."+entry)) {
-			status := helper.MoveFileToDestination(src+file.Name(), dir+"\\"+file.Name())
-			t.AddLine(time.Now().Format("2006/01/02"), time.Now().Format("15:04:05"), entry, src, dir, file.Name(), helper.ByteCountDecimal(fileInfo.Size()), status)
+			status := helper.MoveFileToDestination(
+				src+file.Name(),
+				cm.Dir+"\\"+file.Name(),
+			)
+
+			cm.TabbyWriter.AddLine(
+				time.Now().Format("2006/01/02"),
+				time.Now().Format("15:04:05"),
+				entry,
+				src,
+				cm.Dir,
+				file.Name(),
+				helper.ByteCountDecimal(fileInfo.Size()),
+				status,
+			)
 		}
 	}
-	t.AddLine("")
+	cm.TabbyWriter.AddLine("")
 }
 
-func main() {
-	logging.Log = logging.GetLogger("main")
-
-	fd, _ := os.OpenFile(types.TabbyFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	defer fd.Close()
-
-	w := tabwriter.NewWriter(fd, 0, 0, 4, ' ', 0)
-	t := tabby.NewCustom(w)
-
-	config, sectionNames := getConfSections()
-
-	for _, sectionName := range sectionNames {
-
+func (cm *ConfigurationManager) processSections() {
+	for _, sectionName := range cm.SectionNames {
 		if strings.Contains(sectionName, "global") {
 			continue
 		}
 
-		section, err := config.Section(sectionName)
-
+		section, err := cm.Config.Section(sectionName)
 		if err != nil {
 			logging.Log.Errorf("Failed to fetch section. Error: %s", err.Error())
+			continue
 		}
 
 		src, dst := section.ValueOf("source"), section.ValueOf("destination")
 		entries := strings.Split(section.ValueOf("entries"), ",")
 
 		for _, entry := range entries {
-
-			dir = dst + entry
-			helper.CreateDirectory(dir)
+			cm.Dir = dst + entry
+			helper.CreateDirectory(cm.Dir)
 
 			files, err := os.ReadDir(src)
-
 			if err != nil {
 				logging.Log.Errorf("Failed to read directory: %s. Error: %s.", src, err.Error())
+				continue
 			}
 
-			var countFiles int = 0
+			var countFiles int
 			for _, file := range files {
 				if file.Type().IsRegular() {
 					if strings.HasSuffix(file.Name(), strings.ToUpper("."+entry)) || strings.HasSuffix(file.Name(), strings.ToLower("."+entry)) {
@@ -100,27 +128,38 @@ func main() {
 
 			switch countFiles {
 			case 0:
-				if types.LogType == "logs" {
+				if cm.LogType == "logs" {
 					logging.Log.Infof("No .%s file(s) to move.", entry)
 				} else {
 					logging.Log.Infof(aurora.Sprintf("No .%s file(s) to move.", aurora.Yellow(entry)))
 				}
 			case 1:
-				if types.LogType == "logs" {
+				if cm.LogType == "logs" {
 					logging.Log.Infof("%d file .%s to move", countFiles, entry)
 				} else {
 					logging.Log.Infof(aurora.Sprintf("%d file .%s to move", aurora.Yellow(countFiles), aurora.Yellow(entry)))
 				}
-				moveFileAndAddLineToTabbyLog(files, t, entry, src)
+				cm.moveFileAndAddLineToTabbyLog(files, entry, src)
 			default:
-				if types.LogType == "logs" {
+				if cm.LogType == "logs" {
 					logging.Log.Infof("%d files .%s to move", countFiles, entry)
 				} else {
 					logging.Log.Infof(aurora.Sprintf("%d file .%s to move", aurora.Yellow(countFiles), aurora.Yellow(entry)))
 				}
-				moveFileAndAddLineToTabbyLog(files, t, entry, src)
+				cm.moveFileAndAddLineToTabbyLog(files, entry, src)
 			}
 		}
-
 	}
+}
+
+func main() {
+	logging.Log = logging.GetLogger("main")
+
+	configManager, err := NewConfigManager(types.TabbyFile, types.LogType)
+	if err != nil {
+		logging.Log.Errorf("Failed to initialize ConfigManager. Error: %s", err.Error())
+		return
+	}
+
+	configManager.processSections()
 }
--- types/types.go
+++ types/types.go
@@ -1,7 +1,10 @@
 package types
 
+var (
+	LogType = "logs" // We can set up as "logs" or "terminal"
+)
+
 const (
-	LogType    = "logs" // We can set up as "logs" or "terminal"
 	LogFile    = "D:\\Scripts\\go\\log\\movelooper.log"
 	TabbyFile  = "D:\\Scripts\\go\\log\\movelooper_table_output.log"
 	ConfigFile = "D:\\Scripts\\go\\conf\\movelooper.ini"
